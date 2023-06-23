859e5ad883504307f77f2588b5f448625221c355
Geoff Sowrey <geoff@sowrey.org>
2023-06-23 08:10:07 -0600

    Initial commit of base Gatsby site (WordPress plugin enabled)

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,12 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-wordpress`,
+      options: {
+        url: `https://yoursite.com/graphql`,
+      },
+    }
+  ],
 }
